9ba658c5e4c12c9472b1410a1f32cc16eaba28ef
Tauno Erik <sedumacre@gmail.com>
2021-04-02 22:20:46 +0300

    use penv

--- tools/readkey.py
+++ tools/readkey.py
@@ -11,15 +11,13 @@ from pynput import keyboard
 
 def on_press(key):
     try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
+        print('alphanumeric key {0} pressed'.format(key.char))
+        print('alphanumeric key {0} pressed'.format(KeyCode))
     except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
+        print('special key {0} pressed'.format(key))
 
 def on_release(key):
-    print('{0} released'.format(
-        key))
+    print('{0} released'.format(key))
     if key == keyboard.Key.esc:
         # Stop listener
         return False
